3c5fbad0c5c05320efbaba59dedf6fd7f85229d8
Ayush Patel <ayushpatel.work624@gmail.com>
2026-08-23 14:35:40 +0530

    Time: 23 ms (82.14%), Space: 45.8 MB (96.81%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,56 +1,58 @@
-import java.util.*;
-
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
 
-        int[][][] dp = new int[m][n][n];
+        int[][][] dp = new int[n][m][m];
 
-        for (int i = 0; i < m; i++) {
-            for (int j1 = 0; j1 < n; j1++) {
-                Arrays.fill(dp[i][j1], -1);
+        for (int[][] mat : dp) {
+            for (int[] row : mat) {
+                Arrays.fill(row, -1);
             }
         }
 
-        dp[0][0][n - 1] = grid[0][0] + grid[0][n - 1];
+        dp[0][0][m - 1] = grid[0][0] + grid[0][m - 1];
 
-        for (int i = 1; i < m; i++) {
-            for (int j1 = 0; j1 < n; j1++) {
-                for (int j2 = 0; j2 < n; j2++) {
+        for (int i = 1; i < n; i++) {
 
-                    int best = -1;
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < m; k++) {
+
+                    if (dp[i - 1][j][k] == -1)
+                        continue;
 
                     for (int d1 = -1; d1 <= 1; d1++) {
                         for (int d2 = -1; d2 <= 1; d2++) {
 
-                            int p1 = j1 - d1;
-                            int p2 = j2 - d2;
+                            int pos1 = j + d1;
+                            int pos2 = k + d2;
 
-                            if (p1 >= 0 && p1 < n &&
-                                p2 >= 0 && p2 < n) {
+                            if (pos1 >= 0 && pos1 < m &&
+                                pos2 >= 0 && pos2 < m) {
 
-                                best = Math.max(best, dp[i - 1][p1][p2]);
+                                int cherries = grid[i][pos1];
+
+                                if (pos1 != pos2) {
+                                    cherries += grid[i][pos2];
+                                }
+
+                                dp[i][pos1][pos2] =
+                                    Math.max(
+                                        dp[i][pos1][pos2],
+                                        dp[i - 1][j][k] + cherries
+                                    );
                             }
                         }
                     }
-
-                    if (best == -1)
-                        continue;
-
-                    dp[i][j1][j2] = best + grid[i][j1];
-
-                    if (j1 != j2)
-                        dp[i][j1][j2] += grid[i][j2];
                 }
             }
         }
 
         int ans = 0;
 
-        for (int j1 = 0; j1 < n; j1++) {
-            for (int j2 = 0; j2 < n; j2++) {
-                ans = Math.max(ans, dp[m - 1][j1][j2]);
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < m; k++) {
+                ans = Math.max(ans, dp[n - 1][j][k]);
             }
         }
 
